9e4cef8770becfa101a3cd1eb5110d1a51f17118
Dennis Kats <denniskats43@gmail.com>
2023-03-18 08:51:21 -0400

    Initial Commit

---
+++ PinIt/PinItApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinItApp.swift
+//  PinIt
+//
+//  Created by Dennis Kats on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PinItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
